0ddb1789fab1e7b12f840d281afbf2239d5ec165
Aditya <adityapassi01@gmail.com>
2023-03-06 16:13:25 +0530

    Adding scroll behavior

--- javascript/script.js
+++ javascript/script.js
@@ -2,10 +2,11 @@ window.addEventListener("scroll" , function(){
     let scrollval = window.scrollY;
     if(scrollval < "40"){
         document.getElementById("menu").classList.remove("active");
-        b
+        document.getElementById("top").classList.remove("active")    
     }
 
     else{
         document.getElementById("menu").classList.add("active");
+        document.getElementById("top").classList.add("active");
     }
 })
